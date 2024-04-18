16f54de66e6dbf22163e3ab534f74d44455f69eb
jmonahanATU <123738720+jmonahanATU@users.noreply.github.com>
2024-04-18 13:16:36 +0100

    Links to the 3 workout pages complete

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../App.css'; // Import CSS file for styling
 
 function Dashboard() {
@@ -7,9 +8,15 @@ function Dashboard() {
             <h2 className="dashboard-title">Welcome to the Dashboard</h2>
             <p className="dashboard-text">Explore your fitness journey with our exciting activities:</p>
             <div className="button-container">
-                <button className="dashboard-button boxing-button">Boxing</button>
-                <button className="dashboard-button weights-button">Weights</button>
-                <button className="dashboard-button cardio-button">Cardio</button>
+                <Link to="/boxing">
+                    <button className="dashboard-button boxing-button">Boxing</button>
+                </Link>
+                <Link to="/weights">
+                    <button className="dashboard-button weights-button">Weights</button>
+                </Link>
+                <Link to="/cardio">
+                    <button className="dashboard-button cardio-button">Cardio</button>
+                </Link>
             </div>
         </div>
     );
